c5a656064532fd09fa57582044f70142769d3b18
Ana <ana@Anas-MacBook-Pro.local>
2025-07-09 13:30:16 +0200

    script.js actualizado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class name {
     constructor(parameters) {
-        console.log("This is a class named 'name'");
+        console.log("This is a class named 'name' and has a constructor.");
     }
 }
